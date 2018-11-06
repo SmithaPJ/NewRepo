284d0170ac3d456f6d167d8935f0f9106bb80ed0
sparswan <smitha.parswanatha@optum.com>
2018-11-06 12:31:31 -0500

    Change in the new repo repository for 'LogFliterVersion1' project.

--- LogFliterVersion1/LogFliterVersion1/MainWindow.xaml.cs
+++ LogFliterVersion1/LogFliterVersion1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace LogFliterVersion1
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            label.Content = DateTime.Now.ToString();
+            label.Content = DateTime.Now.ToString()+"Today";
 
         }
     }
